744fbc8a9fec0394eafa6327b5cd68408f6205b5
martinlarsalbert <marale@kth.se>
2018-01-29 22:01:56 +0100

    added a form for Item

--- offer_generator/urls.py
+++ offer_generator/urls.py
@@ -2,8 +2,10 @@ from django.urls import path,re_path
 
 from . import views
 
-app_name = 'polls'
+app_name = 'offer_generator'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path(r'item/', views.item, name='item')
+
 
     ]
--- offer_generator/views.py
+++ offer_generator/views.py
@@ -3,7 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+from .forms import ItemForm
+
+def item(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ItemForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/offer_generator/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ItemForm()
+
+    return render(request, 'offer_generator/item.html', {'form': form})
